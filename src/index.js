2cfe6655da827849e3c9370f6cf02a318bef1765
Diego Villanueva <diegovilla2626@gmail.com>
2023-09-28 22:08:07 -0500

    Add: Error Responses 6.0

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.get('/getWallet', async (req,res) => {
   const { wallet_address } = req.query; 
   try {
       const wallet = await db.one('SELECT wallet_id FROM Wallets WHERE wallet_address = $1', wallet_address);  
-      const get = await getDailyValuesForWallet(wallet);
+      const get = await getDailyValuesForWallet(wallet.wallet_id);
       res.json(get);
     } catch (error) {
       res.status(503).json({ error: error.message });
