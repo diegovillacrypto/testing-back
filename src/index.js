9d0ff77255793b3a5b9a87539f50e0a59942727e
Diego Villanueva <diegovilla2626@gmail.com>
2023-09-28 22:15:48 -0500

    Add: Error Responses 9.0

--- src/index.js
+++ src/index.js
@@ -31,10 +31,10 @@ app.post('/recordWallet', async (req,res) => {
 app.get('/getWallet', async (req,res) => {
   const { wallet_address } = req.query; 
   try {
-      const wallet = await db.one('SELECT wallet_id FROM Wallets WHERE wallet_address = $1', wallet_address);  
+      const wallet = await db.one('SELECT wallet_id FROM Wallets WHERE wallet_address = $1', wallet_address); 
+      console.log(wallet.wallet_id); 
       const get = await getDailyValuesForWallet(wallet.wallet_id);
       res.json(get);
-      res.json(wallet.wallet_id);
     } catch (error) {
       res.status(503).json({ error: error.message });
     }
